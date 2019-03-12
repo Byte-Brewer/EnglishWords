ef3ef5826179023fbc3b07f983e55dbe3ee708cb
Pivowar <Nazar.p@icloud.com>
2019-03-12 03:24:25 +0200

    create UI

--- EnglishWords/ViewController.swift
+++ EnglishWords/ViewController.swift
@@ -9,12 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var leftButtonCollection: [UIButton]!
+    @IBOutlet var rightButtonCollection: [UIButton]!
+    @IBOutlet weak var resetBtn: UIButton!
+    @IBOutlet weak var checkBtn: UIButton!
+    @IBOutlet weak var tryBtn: UIButton!
+    @IBOutlet weak var seeAnswersBtn: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    
+    
+    @IBAction func leftButtonAction(_ sender: UIButton) {
+        print(#function, sender.tag, sender.frame.maxX, sender.frame.maxY)
+    }
+    
+    
+    @IBAction func rightButtonAction(_ sender: UIButton) {
+        print(#function, sender.tag)
+    }
+    
+    @IBAction func resetAction(_ sender: UIButton) {
+        print(#function, sender.tag)
+    }
+    
+    
+    @IBAction func checkAction(_ sender: UIButton) {
+        print(#function, sender.tag)
+    }
+    
+    @IBAction func tryAgainAction(_ sender: UIButton) {
+        print(#function, sender.tag)
+    }
+    
+    @IBAction func seeAnswersActon(_ sender: UIButton) {
+        print(#function, sender.tag)
+    }
+    
+    
 }
 
